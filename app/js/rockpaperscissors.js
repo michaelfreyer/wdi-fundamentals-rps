1313de13020a6bb6c19ca851cc147fdde8576d08
Michael <michael.freyer@gmail.com>
2016-03-22 12:45:13 +0000

    I added the logic for the game - using a switch if statement

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -41,6 +41,82 @@ function getWinner(playerMove,computerMove) {
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
+    switch(playerMove) {
+        case 'rock':
+            
+            if(computerMove === 'paper'){
+            
+            winner = 'computer';
+            }
+            
+            else if (computerMove === 'scissors'){
+                winner = 'player';
+            }
+        
+            else if (computerMove === 'rock'){
+                winner = 'tie';
+            }
+            
+            else {
+              console.log('Please pick a valid move');
+              winner = 'No one'
+            }
+        
+        break;
+
+        case 'paper':
+
+            if(computerMove === 'scissors'){
+            
+            winner = 'computer';
+              
+            }
+            
+            else if (computerMove === 'rock'){
+                winner = 'player';
+            }
+
+            else if (computerMove === 'paper'){
+                winner = 'tie';
+            }
+            
+            else {
+              console.log('Please pick a valid move');
+              winner = 'No one'
+            }
+
+
+
+        break;
+
+        case 'scissors':
+
+        if(computerMove === 'rock'){
+            
+            winner = 'computer';
+        }
+            
+            else if (computerMove === 'paper'){
+                winner = 'player';
+            }
+
+            else if (computerMove === 'rock'){
+                winner = 'tie';
+            }
+            
+            else {
+              console.log('Please pick a valid move');
+              winner = 'No one'
+            }
+        
+
+        break;
+
+        default:
+        console.log('Please pick a valid hand!');
+    }
+
+
     return winner;
 }
 
